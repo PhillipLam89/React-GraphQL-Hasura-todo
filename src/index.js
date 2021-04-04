42ee98bbc73dd845d349eb543f9a8dff023a49e4
Phillip Lam <plam89@ucla.edu>
2021-04-03 22:48:12 -0700

    practicing react

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const greeting = <h3>Hello Rail</h3>
-const rootNode = document.getElementById('root')
+const rootNode = document.querySelector('#root')
 
-ReactDOM.render(greeting,rootNode)
+function Header(props) {
+  return <h1>hello {props.fagName} </h1>
+}
+
+
+function App() {
+  const people = ['phil', 'dave', 'james']
+
+  return (
+
+    <ul>
+      {people.map(person => <Person person={person}/>)}
+    </ul>
+  )
+}
+
+
+  function Person(props) {
+
+      function handlePersonClick(e) {
+          console.log(e.target.innerHTML)
+      }
+
+      return <li style={{cursor: 'pointer'}} onClick={handlePersonClick}>{props.person}</li>
+  }
+
+
+
+ReactDOM.render(<App />,rootNode)
