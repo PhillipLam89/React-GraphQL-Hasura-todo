01f6b674d5fbcc005fbcc6f55cfab3c504a7e4bf
Phillip Lam <plam89@ucla.edu>
2021-04-09 16:14:22 -0700

    minor improvements and state fixes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import CreatePost from './components/CreatePost'
 function App() {
 
   const [user, setUser] = React.useState('PHILLIP')
+  const [posts, setPosts] = React.useState([])
 
   React.useEffect(() => {
       document.title = user ? `${user}'s Feed` : 'Please Login'
@@ -17,7 +18,17 @@ function App() {
 
   return <div>
            <Header user={user} setUser={setUser}/>
-           <CreatePost />
+           <CreatePost user={user} setPosts={setPosts} posts={posts}/>
+           {posts.map((post,index) => (
+            <div key={index}>
+              <br></br>
+              {post.image && (
+                <img src={URL.createObjectURL(post.image)} style={{borderRadius:'10px',height: 100, width: 200, objectFit: 'cover'}} />
+              )}
+                <p>Content: {post.content}</p>
+                <div>Posted by: {user}</div>
+            </div>
+           ))}
          </div>
 }
 
--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,24 +1,32 @@
 import React from 'react'
 
-function CreatePost() {
+function CreatePost(props) {
 
 
     const [content, setContent] = React.useState('')
     const [image, setImage] = React.useState(null)
+    const imageInputRef = React.useRef()
 
+
+    function handleSubmit(e) {
+      e.preventDefault()
+      const post = {content, image, user: props.user}
+      const newPosts = [post, ...props.posts]
+      props.setPosts(newPosts)
+      setContent('')  //this along with value attribute will make this input a controlled element by state
+      imageInputRef.current.value = '' //refs must be used for inputs with type file since controlled state wont work
+    }
 
   return (
     <div>
       <h1>Create New Post</h1>
-      <form>
-        <input onChange={e =>setContent(e.target.value)} type="text" placeholder="add Content"></input>
-        <input onChange={e =>setImage(e.target.files[0])} type="file"></input>
+      <form onSubmit={handleSubmit}>
+        <input value={content} onChange={e =>setContent(e.target.value)} type="text" placeholder="add Content"></input>
+        <input ref={imageInputRef} onChange={e =>setImage(e.target.files[0])} type="file"></input>
         <br></br>
         <br></br>
         <button type="submit">Submit Post</button>
       </form>
-      <p>{content}</p>
-      {image && <img style={{height: 100, width: 200, objectFit: 'cover'}} src={URL.createObjectURL(image)} />}
     </div>
   )
 }
