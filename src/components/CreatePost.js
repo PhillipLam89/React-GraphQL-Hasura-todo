e8e0e765aa151a938aa9480e131787024859c913
Phillip Lam <plam89@ucla.edu>
2021-04-08 21:37:34 -0700

    component files added

---
+++ src/components/CreatePost.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CreatePost() {
+  return (
+    <div>Create Posts</div>
+  )
+}
+
+export default CreatePost
--- src/index.js
+++ src/index.js
@@ -1,34 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App'
 
-const rootNode = document.querySelector('#root')
-
-function Header(props) {
-  return <h1>hello {props.fagName} </h1>
-}
-
-
-function App() {
-  const people = ['phil', 'dave', 'james']
-
-  return (
-
-    <ul>
-      {people.map(person => <Person person={person}/>)}
-    </ul>
-  )
-}
-
-
-  function Person(props) {
-
-      function handlePersonClick(e) {
-          console.log(e.target.innerHTML)
-      }
-
-      return <li style={{cursor: 'pointer'}} onClick={handlePersonClick}>{props.person}</li>
-  }
 
 
 
+const rootNode = document.querySelector('#root')
 ReactDOM.render(<App />,rootNode)
