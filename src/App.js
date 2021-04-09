28ddcce93684ee2d4a63ae03149eb40dc9b223a8
Phillip Lam <plam89@ucla.edu>
2021-04-09 14:23:33 -0700

    user able to upload images, texts typed are displayed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Login from './components/Login'
 import Header from './components/Header'
+import CreatePost from './components/CreatePost'
 
 function App() {
 
- const [user, setUser] = React.useState('')
+  const [user, setUser] = React.useState('PHILLIP')
+
+  React.useEffect(() => {
+      document.title = user ? `${user}'s Feed` : 'Please Login'
+  }, [user])
 
   if (!user) {
       return <Login setUser={setUser}/>
@@ -12,6 +17,7 @@ function App() {
 
   return <div>
            <Header user={user} setUser={setUser}/>
+           <CreatePost />
          </div>
 }
 
--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,8 +1,25 @@
 import React from 'react'
 
 function CreatePost() {
+
+
+    const [content, setContent] = React.useState('')
+    const [image, setImage] = React.useState(null)
+
+
   return (
-    <div>Create Posts</div>
+    <div>
+      <h1>Create New Post</h1>
+      <form>
+        <input onChange={e =>setContent(e.target.value)} type="text" placeholder="add Content"></input>
+        <input onChange={e =>setImage(e.target.files[0])} type="file"></input>
+        <br></br>
+        <br></br>
+        <button type="submit">Submit Post</button>
+      </form>
+      <p>{content}</p>
+      {image && <img style={{height: 100, width: 200, objectFit: 'cover'}} src={URL.createObjectURL(image)} />}
+    </div>
   )
 }
 
