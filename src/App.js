fc49352a53228fd98546e06f61669844eef27ff9
Phillip Lam <plam89@ucla.edu>
2021-04-11 22:52:38 -0700

    used useReducer instead of React callback for better performance

--- src/App.js
+++ src/App.js
@@ -3,32 +3,35 @@ import Login from './components/Login'
 import Header from './components/Header'
 import CreatePost from './components/CreatePost'
 import PostList from './components/PostList'
+import postReducer from './reducer'
 
 export const UserContext = React.createContext()
+export const PostContext = React.createContext({posts: []})
 
 function App() {
-
+  const initialPostState = React.useContext(PostContext)
+  const [state, dispatch] =  React.useReducer(postReducer, initialPostState)
   const [user, setUser] = React.useState('PHILLIP')
-  const [posts, setPosts] = React.useState([])
-  const [count, setCount] = React.useState(0)
+  // const [posts, setPosts] = React.useState([])
+
 
   React.useEffect(() => {
       document.title = user ? `${user}'s Feed` : 'Please Login'
   }, [user])
 
-  function handleAddPost(newPost) {
-    setPosts([newPost, ...posts])
-  }
 
   if (!user) {
       return <Login setUser={setUser}/>
   }
 
-  return <UserContext.Provider value={user} style={{padding: '1rem'}}>
-           <Header user={user} setUser={setUser}/>
-           <CreatePost user={user} handleAddPost={handleAddPost} />
-           <PostList posts={posts}/>
-         </UserContext.Provider> //Context provider allows us to pass the 'user' prop down 2 levels directly to Post.js component. NO NEED 2 PROP DRILLZ
+  return (
+        <PostContext.Provider value={{state, dispatch}}>
+          <UserContext.Provider value={user} style={{padding: '1rem'}}>
+            <Header user={user} setUser={setUser}/>
+            <CreatePost user={user}  />
+            <PostList posts={state.posts}/>
+          </UserContext.Provider>
+        </PostContext.Provider>)
 }
 
 export default App
--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,21 +1,21 @@
 import React from 'react'
+import {PostContext} from '../App'
 
 function CreatePost(props) {
 
+    const {dispatch} = React.useContext(PostContext)
 
     const [content, setContent] = React.useState('')
     const [image, setImage] = React.useState(null)
 
 
     const imageInputRef = React.useRef()
-
-
     function handleSubmit(e) {
       e.preventDefault()
+
       const post = {content, image, user: props.user}
-      props.handleAddPost(post)
-      // const newPosts = [post, ...props.posts]
-      // props.setPosts(prevPosts => [post, ...prevPosts])
+      dispatch({type: 'ADD_POST', payload: {post}})
+
       setContent('') //this along with value attribute will make this input a controlled element by state
       imageInputRef.current.value = '' //refs must be used for inputs with type file since controlled state wont work
       imageInputRef.current.files = setImage('') //resets internal image selection. If not, clicking submit will post previous img even if value is ''
@@ -25,7 +25,7 @@ function CreatePost(props) {
     <div>
       <h1>Create New Post</h1>
       <form onSubmit={handleSubmit}>
-        <input value={content} onChange={e =>setContent(e.target.value)} type="text" placeholder="add Content"></input>
+        <input required value={content} onChange={e =>setContent(e.target.value)} type="text" placeholder="add Content"></input>
         <input ref={imageInputRef} onChange={e =>setImage(e.target.files[0])} type="file"></input>
         <br></br>
         <br></br>
