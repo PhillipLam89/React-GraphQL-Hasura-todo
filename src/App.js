ef60dc65fcb4e50cb29e52a358dbfd2c46b80555
Phillip Lam <plam89@ucla.edu>
2021-04-20 22:54:21 -0700

    user now able to cross off todo checklists using GraphQL

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import {useQuery} from '@apollo/react-hooks'
+import {useQuery, useMutation} from '@apollo/react-hooks'
 import {gql} from 'apollo-boost'
+
 
 const GET_TODOS = gql`
 query getTodos {
@@ -11,9 +12,27 @@ query getTodos {
   }
 }
 `
+const TOGGLE_TODO = gql`
+mutation toggleTodo($id: uuid!, $done: Boolean!) {
+  update_todos(where: {id: {_eq: $id}}, _set: {done: $done}) {
+    returning {
+      done
+      id
+      text
+    }
+  }
+}
+`
 
 function App() {
   const {data, loading, error} = useQuery(GET_TODOS)
+  const [toggleTodo] =  useMutation(TOGGLE_TODO)
+
+  async function handleToggleTodo({id, done}) {
+   const data = await toggleTodo({variables: {id, done: !done}})
+    console.log(data)
+  }
+
   if (error) return <h1>Error while fetching data...</h1>
   return loading ? <h1>Loading...</h1> :  (
 
@@ -22,22 +41,23 @@ function App() {
           <span role="img" aria-label="Checkmark">✔️</span>
       </h1>
 
-      <form>
-        <input
+      <form className="mb3">
+        <input className="pa2 f4 b--dashed"
           type="text"
           placeholder="write your todo"
          />
-         <button type="submit">Create</button>
+         <button className="pa2 f4 bg-green" type="submit">Create</button>
       </form>
-
-      {data.todos.map(todo => (
-      <p key={todo.id}>
-        <span>
-          {todo.text}
-        </span>
-        <button style={{marginLeft: '.5rem'}}>&times;</button>
-      </p>
-      ))}
+        <div className="flex items-center justify-center flex-column">
+          {data.todos.map(todo => (
+          <p onDoubleClick={() => handleToggleTodo(todo)} key={todo.id}>
+            <span className={`pointer list pa1 f3 ${todo.done && 'strike'}`}>
+              {todo.text}
+            </span>
+            <button className="bg-transparent bn f4" style={{marginLeft: '.5rem', color: 'red', transform: 'scale(2)'}}>&times;</button>
+          </p>
+          ))}
+       </div>
     </div>
   )
 }
